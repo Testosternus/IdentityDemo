1667dc8a990853ff829dac666117468aa1edd30e
Jonathan Boydens <jonathan_boydens@hotmail.com>
2019-10-22 14:03:18 +0200

    add example Razor Page

---
+++ IdentityDemo/Pages/Voorbeeld.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityDemo.Pages
+{
+    public class VoorbeeldModel : PageModel
+    {
+        public string Message { get; private set; } = "Vanuit de pagina in C#! ";
+        public void OnGet()
+        {
+            Message += $"Het is nu {DateTime.Now}.";
+        }
+    }
+}
